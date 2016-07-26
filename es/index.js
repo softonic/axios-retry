aa507ae36b9ee0f78d0cf1bedbcdbd708f28fe9e
Rubén Norte <ruben.norte@softonic.com>
2016-07-26 16:51:25 +0200

    Initial implementation

---
+++ es/index.js
@@ -0,0 +1,55 @@
+import isRetryAllowed from 'is-retry-allowed';
+
+/**
+ * Adds response interceptors to an axios instance to retry requests failed due to network issues
+ *
+ * @example
+ *
+ * import axios from 'axios';
+ *
+ * axiosRetry(axios, { retries: 3 });
+ *
+ * axios.get('http://example.com/test') // The first request fails and the second returns 'ok'
+ *   .then(result => {
+ *     result.data; // 'ok'
+ *   });
+ *
+ * // Also works with custom axios instances
+ * const client = axios.create({ baseURL: 'http://example.com' });
+ * axiosRetry(client, { retries: 3 });
+ *
+ * client.get('/test') // The first request fails and the second returns 'ok'
+ *   .then(result => {
+ *     result.data; // 'ok'
+ *   });
+ *
+ * @param {Axios} axios An axios instance (the axios object or one created from axios.create)
+ * @param {Object} [options]
+ * @param {number} [options.retries=3] Number of retries
+ */
+export default function axiosRetry(axios, { retries = 3 } = {}) {
+  axios.interceptors.response.use(null, error => {
+    const config = error.config;
+
+    // If we have no information to retry the request
+    if (!config) {
+      return Promise.reject(error);
+    }
+
+    config.retryCount = config.retryCount || 0;
+
+    if (!error.response && config.retryCount < retries && isRetryAllowed(error)) {
+      config.retryCount++;
+
+      // Axios fails merging this configuration to the default configuration because it has an issue
+      // with circular structures
+      if (axios.defaults.agent === config.agent) {
+        delete config.agent;
+      }
+
+      return axios(config);
+    }
+
+    return Promise.reject(error);
+  });
+}
---
+++ spec/index.spec.js
@@ -0,0 +1,169 @@
+import http from 'http';
+import nock from 'nock';
+import axios from 'axios';
+import axiosRetry from '../es/index';
+
+const NETWORK_ERROR = new Error('Some connection error');
+NETWORK_ERROR.code = 'ECONNRESET';
+
+function setupResponses(client, responses) {
+  const configureResponse = () => {
+    const response = responses.shift();
+    if (response) {
+      response();
+    }
+  };
+  client.interceptors.response.use(result => {
+    configureResponse();
+    return result;
+  }, error => {
+    configureResponse();
+    return Promise.reject(error);
+  });
+  configureResponse();
+}
+
+describe('axiosRetry(axios, { retries })', () => {
+  afterEach(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+
+  it('should resolve with a succesful response', done => {
+    const client = axios.create();
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').reply(200, 'It worked!')
+    ]);
+
+    axiosRetry(client, { retries: 0 });
+
+    client.get('http://example.com/test').then(result => {
+      expect(result.status).toBe(200);
+      done();
+    }, done.fail);
+  });
+
+  it('should resolve with a succesful response after an error', done => {
+    const client = axios.create();
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').replyWithError(NETWORK_ERROR),
+      () => nock('http://example.com').get('/test').reply(200, 'It worked!')
+    ]);
+
+    axiosRetry(client, { retries: 1 });
+
+    client.get('http://example.com/test').then(result => {
+      expect(result.status).toBe(200);
+      done();
+    }, done.fail);
+  });
+
+  it('should resolve with a succesful response after an error (with agent)', done => {
+    const httpAgent = new http.Agent();
+
+    // Simulate circular structure
+    const fakeSocket = { foo: 'foo' };
+    httpAgent.sockets['multisearch.api.softonic.com:80:'] = [fakeSocket];
+    fakeSocket.socket = fakeSocket;
+
+    const client = axios.create({ agent: httpAgent });
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').replyWithError(NETWORK_ERROR),
+      () => nock('http://example.com').get('/test').reply(200, 'It worked!')
+    ]);
+
+    axiosRetry(client, { retries: 1 });
+
+    client.get('http://example.com/test').then(result => {
+      expect(result.status).toBe(200);
+      done();
+    }, done.fail);
+  });
+
+  it('should reject with a request error if retries <= 0', done => {
+    const client = axios.create();
+
+    const generatedError = new Error();
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').replyWithError(generatedError)
+    ]);
+
+    axiosRetry(client, { retries: 0 });
+
+    client.get('http://example.com/test').then(done.fail, error => {
+      expect(error).toBe(generatedError);
+      done();
+    });
+  });
+
+  it('should reject with a request error if there are more errors than retries', done => {
+    const client = axios.create();
+
+    const generatedError = new Error();
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').replyWithError(new Error('foo error')),
+      () => nock('http://example.com').get('/test').replyWithError(generatedError)
+    ]);
+
+    axiosRetry(client, { retries: 1 });
+
+    client.get('http://example.com/test').then(done.fail, error => {
+      expect(error).toBe(generatedError);
+      done();
+    });
+  });
+
+  it('should reject with error responses (404, 500, etc.) without retrying', done => {
+    const client = axios.create();
+
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').reply(404, 'Not found'),
+      () => nock('http://example.com').get('/test').reply(200)
+    ]);
+
+    axiosRetry(client, { retries: 1 });
+
+    client.get('http://example.com/test').then(done.fail, error => {
+      expect(error.response.status).toBe(404);
+      done();
+    });
+  });
+
+  it('should reject with blacklisted errors without retrying', done => {
+    const client = axios.create();
+
+    const notFoundError = new Error('Not Found');
+    notFoundError.code = 'ENOTFOUND';
+
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').replyWithError(notFoundError),
+      () => nock('http://example.com').get('/test').reply(200)
+    ]);
+
+    axiosRetry(client, { retries: 1 });
+
+    client.get('http://example.com/test').then(done.fail, error => {
+      expect(error).toBe(notFoundError);
+      done();
+    });
+  });
+
+  it('should reject with errors without a config property without retrying', done => {
+    const client = axios.create();
+
+    setupResponses(client, [
+      () => nock('http://example.com').get('/test').replyWithError(new Error()),
+      () => nock('http://example.com').get('/test').reply(200)
+    ]);
+
+    const generatedError = new Error();
+    client.interceptors.response.use(null, () => Promise.reject(generatedError));
+
+    axiosRetry(client, { retries: 1 });
+
+    client.get('http://example.com/test').then(done.fail, error => {
+      expect(error).toBe(generatedError);
+      done();
+    });
+  });
+});
