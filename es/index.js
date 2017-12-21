deb35cf15439ae9e801c4d5c33758a2220f0d480
David Madden <d.d.madden@bham.ac.uk>
2017-12-21 09:33:45 +0000

    Updated parameter type

--- es/index.js
+++ es/index.js
@@ -149,7 +149,7 @@ function fixConfig(axios, config) {
  * @param {Axios} axios An axios instance (the axios object or one created from axios.create)
  * @param {Object} [defaultOptions]
  * @param {number} [defaultOptions.retries=3] Number of retries
- * @param {number} [defaultOptions.retryCondition=isNetworkOrIdempotentRequestError]
+ * @param {Function} [defaultOptions.retryCondition=isNetworkOrIdempotentRequestError]
  *        A function to determine if the error can be retried
  */
 export default function axiosRetry(axios, defaultOptions) {
