063c8159baffb01796a4bba2c5103536c26fb397
Calvin Moore <calmoore@redventures.com>
2019-08-21 12:17:08 -0400

    Fixed export.

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,6 @@
 import * as axios from 'axios'
 
-export interface IAxiosRetryConfig {
-  /**
-   * The number of times to retry before failing
-   * default: 3
-   *
-   * @type {number}
-   */
-  retries?: number,
-  /**
-   * Defines if the timeout should be reset between retries
-   * default: false
-   *
-   * @type {boolean}
-   */
-  shouldResetTimeout?: boolean,
-  /**
-   * A callback to further control if a request should be retried. By default, it retries if the result did not have a response.
-   * default: error => !error.response
-   *
-   * @type {Function}
-   */
-  retryCondition?: (error: axios.AxiosError) => boolean,
-  /**
-   * A callback to further control the delay between retry requests. By default there is no delay.
-   *
-   * @type {Function}
-   */
-  retryDelay?: (retryCount: number, error: axios.AxiosError) => number
-}
-
-export interface IAxiosRetry {
+interface IAxiosRetry {
   (
     axios: axios.AxiosStatic | axios.AxiosInstance,
     axiosRetryConfig?: IAxiosRetryConfig
@@ -44,4 +14,38 @@ export interface IAxiosRetry {
   exponentialDelay(retryNumber: number): number;
 }
 
-export declare const axiosRetry: IAxiosRetry;
+declare namespace IAxiosRetry {
+  export interface IAxiosRetryConfig {
+    /**
+     * The number of times to retry before failing
+     * default: 3
+     *
+     * @type {number}
+     */
+    retries?: number,
+    /**
+     * Defines if the timeout should be reset between retries
+     * default: false
+     *
+     * @type {boolean}
+     */
+    shouldResetTimeout?: boolean,
+    /**
+     * A callback to further control if a request should be retried. By default, it retries if the result did not have a response.
+     * default: error => !error.response
+     *
+     * @type {Function}
+     */
+    retryCondition?: (error: axios.AxiosError) => boolean,
+    /**
+     * A callback to further control the delay between retry requests. By default there is no delay.
+     *
+     * @type {Function}
+     */
+    retryDelay?: (retryCount: number, error: axios.AxiosError) => number
+  }
+}
+
+declare const axiosRetry: IAxiosRetry;
+
+export = axiosRetry;
