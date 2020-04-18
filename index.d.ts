3d1f37620ba706d192e644c274e70297c6174fde
Calvin Moore <c1moore@outlook.com>
2020-04-18 14:19:12 -0400

    Fixed TS definitions. (#92)
    * Fixed TS definitions.
    
    * Fixed export.
    
    * Bumped version.

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,51 @@
 import * as axios from 'axios'
 
-export interface IAxiosRetryConfig {
-  /**
-   * The number of times to retry before failing
-   * default: 3
-   *
-   * @type {number}
-   */
-  retries?: number,
-  /**
-   * Defines if the timeout should be reset between retries
-   * default: false
-   *
-   * @type {boolean}
-   */
-  shouldResetTimeout?: boolean,
-  /**
-   * A callback to further control if a request should be retried. By default, it retries if the result did not have a response.
-   * default: error => !error.response
-   *
-   * @type {Function}
-   */
-  retryCondition?: (error: axios.AxiosError) => boolean,
-  /**
-   * A callback to further control the delay between retry requests. By default there is no delay.
-   *
-   * @type {Function}
-   */
-  retryDelay?: (retryCount: number, error: axios.AxiosError) => number
-}
-
-export interface IAxiosRetry {
+interface IAxiosRetry {
   (
     axios: axios.AxiosStatic | axios.AxiosInstance,
     axiosRetryConfig?: IAxiosRetryConfig
-  ): void
+  ): void;
+
+  isNetworkError(error: Error): boolean;
+  isRetryableError(error: Error): boolean;
+  isSafeRequestError(error: Error): boolean;
+  isIdempotentRequestError(error: Error): boolean;
+  isNetworkOrIdempotentRequestError(error: Error): boolean;
+  exponentialDelay(retryNumber: number): number;
 }
 
-declare const axiosRetry: IAxiosRetry
+declare namespace IAxiosRetry {
+  export interface IAxiosRetryConfig {
+    /**
+     * The number of times to retry before failing
+     * default: 3
+     *
+     * @type {number}
+     */
+    retries?: number,
+    /**
+     * Defines if the timeout should be reset between retries
+     * default: false
+     *
+     * @type {boolean}
+     */
+    shouldResetTimeout?: boolean,
+    /**
+     * A callback to further control if a request should be retried. By default, it retries if the result did not have a response.
+     * default: error => !error.response
+     *
+     * @type {Function}
+     */
+    retryCondition?: (error: axios.AxiosError) => boolean,
+    /**
+     * A callback to further control the delay between retry requests. By default there is no delay.
+     *
+     * @type {Function}
+     */
+    retryDelay?: (retryCount: number, error: axios.AxiosError) => number
+  }
+}
 
-export default axiosRetry
+declare const axiosRetry: IAxiosRetry;
 
-export function isNetworkError(error: Error): boolean;
-export function isRetryableError(error: Error): boolean;
-export function isSafeRequestError(error: Error): boolean;
-export function isIdempotentRequestError(error: Error): boolean;
-export function isNetworkOrIdempotentRequestError(error: Error): boolean;
-export function exponentialDelay(retryNumber: number): number;
+export = axiosRetry;
